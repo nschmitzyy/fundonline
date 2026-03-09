0e3f23fa9a1118d8e76a22d6becc7322ccd93088
nschmitzyy <schmitzniklas08@gmail.com>
2026-03-09 14:37:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,36 +2,6 @@ import streamlit as st
 import pandas as pd
 from supabase_client import supabase
 
-import numpy as np
-from keras.models import load_model
-from PIL import Image, ImageOps
-
-np.set_printoptions(suppress=True)
-
-# Modell laden
-model = load_model("keras_Model.h5", compile=False)
-class_names = open("labels.txt", "r").readlines()
-
-def predict_image(image):
-
-    size = (224, 224)
-
-    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
-    image_array = np.asarray(image)
-
-    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
-
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    data[0] = normalized_image_array
-
-    prediction = model.predict(data)
-    index = np.argmax(prediction)
-
-    class_name = class_names[index][2:].strip()
-    confidence_score = prediction[0][index]
-
-    return class_name, confidence_score
-
 st.title("🔎 Online Fundbüro")
 
 menu = st.sidebar.selectbox(
